2144d4817e5a8fa96421fc9e01a6f3c19f789d42
X00191169 Azubuike Ibe <x00191169@TA.IT-TALLAGHT.IE>
2022-09-29 16:15:49 +0100

    Initial commit

---
+++ src/lab2/Exercise1.java
@@ -0,0 +1,13 @@
+package lab2;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Double distanceKm;
+        Double petrolCostPerKm;
+        Double journeyCost;
+        String km;
+        distanceKm = 100.0;
+        petrolCostPerKm = 50.0;
+
+        journeyCost = distanceKm * petrolCostPerKm;
+}
